a4679e76a67dd6bb707c164a6d3691f65a8aaab8
CKiilu <cndeti@gmail.com>
2016-03-12 16:10:00 +0300

    Added API key

---
+++ app/src/main/java/com/scurrae/chris/videotube/Config.java
@@ -0,0 +1,11 @@
+package com.scurrae.chris.videotube;
+
+/**
+ * Created by chris on 3/12/16.
+ */
+public final class Config {
+    private Config(){
+
+    }
+    public static final String YOUTUBE_API_KEY = "AIzaSyCpj8W2Hirsbt-5yUrR5gDEkJbA0_rfZkU";
+}
