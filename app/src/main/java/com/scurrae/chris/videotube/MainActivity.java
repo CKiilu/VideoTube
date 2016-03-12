46c466a94b0b8d114c74ca246c6e2cdc2da31faf
CKiilu <cndeti@gmail.com>
2016-03-12 16:32:20 +0300

    Initialization error handling

--- app/src/main/java/com/scurrae/chris/videotube/MainActivity.java
+++ app/src/main/java/com/scurrae/chris/videotube/MainActivity.java
@@ -1,13 +1,48 @@
 package com.scurrae.chris.videotube;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Initialize player object
+        youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtube_view);
+        youTubePlayerView.initialize(Config.YOUTUBE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        // If initialization succeeds
+        if (!b){
+            youTubePlayer.cueVideo("t2NgsJrrAyM");
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        // If initialization fails
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else{
+            String error = String.format(getString(R.string.player_error), youTubeInitializationResult.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
     }
 }
